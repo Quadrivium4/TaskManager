98b7c311e81e128f43f6668ec9bf4634713c069a
Quadrivium <coder.quadrivium4@gmail.com>
2022-06-16 14:42:36 +0200

    commit

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const app = express()
 app.use(express.static("public"))
 
 // define the first route
-app.get("/", function (req, res) {
+/*app.get("/", function (req, res) {
     res.send("<h1>Hello World!</h1>")
-})
+})*/
 
 // start the server listening for requests
 app.listen(process.env.PORT || 3000, 
