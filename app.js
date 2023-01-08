d80664e6e5e9dfa00407554efdd3fe1f2ce250e4
Quadrivium <coder.quadrivium4@gmail.com>
2023-01-08 14:57:15 +0100

    new commit

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const fs = require("fs");;
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+console.log(process.env.MONGO_URI)
 const mongo_uri = process.env.MONGO_URI || "mongodb://localhost:27017";
 const tasks = require("./routes/tasks");
 const notFound = require("./middleware/404");
@@ -16,11 +17,13 @@ app.use(notFound);
 
 const start = async () =>{
     try{
+        console.log("goes", process.env.MONGO_URI)
         await connectDB(mongo_uri);
         app.listen(port,()=>{
             console.log(`server is listening on port ${port}...`)
         });
     }catch(error){
+        console.log("hi", process.env.MONGO_URI)
         console.log(error)
     }
 }
